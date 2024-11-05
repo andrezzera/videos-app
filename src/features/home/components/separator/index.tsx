d4f42bd1704a1970c0c6bd22acb0a581444f9d3e
André Paulo <andrezzera@outlook.com>
2024-11-05 15:54:57 -0300

    feat: create video separator component

---
+++ src/features/home/components/separator/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const Separator = () => {
+  return <S.Line />;
+};
+
+export default Separator;
---
+++ src/features/home/components/separator/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Line = styled.View`
+  width: 100%;
+  height: 1px;
+  background: #eee;
+  margin: 16px 0;
+`;
