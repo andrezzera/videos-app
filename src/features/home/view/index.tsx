53e30ab8cf51696b8d64875fb6e2f0bba771f861
André Paulo <andrezzera@outlook.com>
2024-11-11 11:02:51 -0300

    feat: add animated screen title

--- src/features/home/view/index.tsx
+++ src/features/home/view/index.tsx
@@ -1,16 +1,32 @@
-import { FlatList } from "react-native";
+import { Animated, FlatList } from "react-native";
 import * as S from "./styles";
 import Video from "../components/video";
 import { useVideos } from "../hooks/videos.hook";
 import VideoSkeleton from "../components/video-skeleton";
+import { useEffect, useRef } from "react";
 
 export const HomeView = () => {
   const { videos, fetchVideos, hasNextPage } = useVideos();
+  const titlePosition = useRef(new Animated.ValueXY({ x: -150, y: 0 })).current;
+
+  useEffect(() => {
+    Animated.spring(titlePosition, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: true,
+    }).start();
+  }, [titlePosition]);
 
   return (
     <S.Container>
+      <S.ScreenTitle
+        style={{
+          transform: [{ translateX: titlePosition.x }, { translateY: titlePosition.y }],
+        }}
+        as={Animated.Text}>
+        Feed de videos
+      </S.ScreenTitle>
       {videos && (
-        <FlatList
+        <Animated.FlatList
           data={videos}
           renderItem={({ item }) => (
             <Video
--- src/features/home/view/styles.ts
+++ src/features/home/view/styles.ts
@@ -4,3 +4,10 @@ export const Container = styled.View`
   flex: 1;
   background: ${({ theme }) => theme.colors.background};
 `;
+
+export const ScreenTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 20px;
+  margin-bottom: 16px;
+`;
